7e5cb417073d7e28f4a65bcdd51ebe7972f66844
GIANG NHAT TRUONG <giangnhattruong@gmail.com>
2022-07-08 11:14:57 +0700

    First commit

---
+++ src/com/designpattern/memento/EditorHistory.java
@@ -0,0 +1,20 @@
+package com.designpattern.memento;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class EditorHistory {
+	private List<EditorState> states;
+	
+	public void EditorState() {
+		states = new LinkedList<>();
+	}
+	
+	public void push(EditorState state) {
+		states.add(state);
+	}
+	
+	public EditorState pop() {
+		return states.remove(states.size() - 1);
+	}
+}
